c8623a109811203920c667f3c7e6392844f02be5
Brian <brian.cohen@edu.gva.es>
2026-05-22 12:04:41 +0200

    acabdo

--- rogue-guild/src/main/java/org/sopra/rogueguild/repository/model/Incursion.java
+++ rogue-guild/src/main/java/org/sopra/rogueguild/repository/model/Incursion.java
@@ -6,51 +6,56 @@ public class Incursion {
     private int goldReward;
     private Item itemREward;
     private String description, shortName;
-    private Player player;
-    private ItemGenerator itemGenerator;
+    private ItemGenerator itemGenerator = new ItemGenerator();
     private Random random = new Random();
     private int id;
 
-    public Incursion(int goldReward, Item itemREward, String description, String shortName) {
-        this.goldReward = (int) (Math.round(goldReward / 5.0) * 5);
-        this.itemREward = itemREward;
+    public Incursion(String description, String shortName) {
+        if (description == null || description.isBlank() || shortName == null || shortName.isBlank()) {
+            throw new IllegalArgumentException("La descripción y el nombre corto no pueden estar vacíos.");
+        }
+
         this.description = description;
         this.shortName = shortName;
     }
 
-    public void iniciarIncursion() {
+    public void iniciarIncursion(Player player) {
         int opcionRecompesa = random.nextInt(3) + 1;
         Item itemMenor;
 
         switch (opcionRecompesa) {
             case 1:
                 recompensaMenor();
-                player.setGold(player.getGold() + goldReward);
+                this.goldReward = (int) (Math.round(this.goldReward / 5.0) * 5);
+                player.setGold(player.getGold() + this.goldReward);
                 id = random.nextInt(100) + 1;
-                player.addItem(id, itemREward);
+                player.addItem(this.id, this.itemREward);
                 break;
             case 2:
                 recompensaItem();
+                this.goldReward = 0;
                 id = random.nextInt(100) + 1;
-                player.addItem(id, itemREward);
+                player.addItem(id, this.itemREward);
                 break;
             case 3:
+                this.itemREward = null;
                 recompensaGold();
-                player.setGold(player.getGold() + goldReward);
+                goldReward = (int) (Math.round(this.goldReward / 5.0) * 5);
+                player.setGold(player.getGold() + this.goldReward);
 
                 break;
         }
     }
 
     private void recompensaMenor() {
-        int goldReward = random.nextInt(100) + 1;
+        this.goldReward = random.nextInt(100) + 1;
         Item itemMenor;
 
         do {
             itemMenor = itemGenerator.randomItemGenerator();
         } while (itemMenor.getBasePrice() > 50);
 
-        itemREward = itemMenor;
+        this.itemREward = itemMenor;
     }
 
     private void recompensaItem() {
@@ -60,11 +65,11 @@ public class Incursion {
             itemMayor = itemGenerator.randomItemGenerator();
         } while (itemMayor.getBasePrice() < 50);
 
-        itemREward = itemMayor;
+        this.itemREward = itemMayor;
     }
 
     private void recompensaGold() {
-        int goldReward = random.nextInt(3000) + 1;
+        this.goldReward = random.nextInt(3000) + 1;
     }
 
 }
