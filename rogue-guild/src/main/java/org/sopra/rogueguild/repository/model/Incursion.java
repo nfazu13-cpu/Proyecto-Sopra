0f5e2a864c9aec01e293471e5d5857e9d3d2e559
Brian <brian.cohen@edu.gva.es>
2026-05-22 11:36:53 +0200

    incursion casi acabado

--- rogue-guild/src/main/java/org/sopra/rogueguild/repository/model/Incursion.java
+++ rogue-guild/src/main/java/org/sopra/rogueguild/repository/model/Incursion.java
@@ -1,10 +1,15 @@
 package org.sopra.rogueguild.repository.model;
 
+import java.util.Random;
+
 public class Incursion {
     private int goldReward;
     private Item itemREward;
     private String description, shortName;
     private Player player;
+    private ItemGenerator itemGenerator;
+    private Random random = new Random();
+    private int id;
 
     public Incursion(int goldReward, Item itemREward, String description, String shortName) {
         this.goldReward = (int) (Math.round(goldReward / 5.0) * 5);
@@ -13,8 +18,53 @@ public class Incursion {
         this.shortName = shortName;
     }
 
-    public void recompensaMenor() {
+    public void iniciarIncursion() {
+        int opcionRecompesa = random.nextInt(3) + 1;
+        Item itemMenor;
 
+        switch (opcionRecompesa) {
+            case 1:
+                recompensaMenor();
+                player.setGold(player.getGold() + goldReward);
+                id = random.nextInt(100) + 1;
+                player.addItem(id, itemREward);
+                break;
+            case 2:
+                recompensaItem();
+                id = random.nextInt(100) + 1;
+                player.addItem(id, itemREward);
+                break;
+            case 3:
+                recompensaGold();
+                player.setGold(player.getGold() + goldReward);
+
+                break;
+        }
+    }
+
+    private void recompensaMenor() {
+        int goldReward = random.nextInt(100) + 1;
+        Item itemMenor;
+
+        do {
+            itemMenor = itemGenerator.randomItemGenerator();
+        } while (itemMenor.getBasePrice() > 50);
+
+        itemREward = itemMenor;
+    }
+
+    private void recompensaItem() {
+        Item itemMayor;
+
+        do {
+            itemMayor = itemGenerator.randomItemGenerator();
+        } while (itemMayor.getBasePrice() < 50);
+
+        itemREward = itemMayor;
+    }
+
+    private void recompensaGold() {
+        int goldReward = random.nextInt(3000) + 1;
     }
 
 }
--- rogue-guild/src/main/java/org/sopra/rogueguild/repository/model/ItemGenerator.java
+++ rogue-guild/src/main/java/org/sopra/rogueguild/repository/model/ItemGenerator.java
@@ -5,21 +5,21 @@ import java.util.List;
 
 public class ItemGenerator {
 
-    private final String[] WEAPON_PREFIXES = {"Espada", "Hacha", "Daga", "Lanza", "Mandoble", "Arco", "Maza", "Bastón"};
-    private final String[] ARMOR_PREFIXES = {"Armadura", "Cota", "Peto", "Coraza", "Malla"};
-    private final String[] POTION_PREFIXES = {"Poción", "Elixir", "Brebaje", "Ungüento", "Tintura"};
-    private final String[] HELMET_PREFIXES = {"Yelmo", "Casco", "Celada", "Capucha", "Visera"};
-    private final String[] BOOTS_PREFIXES = {"Botas", "Grebas", "Sandalias", "Escarpines"};
+    private final String[] WEAPON_PREFIXES = { "Espada", "Hacha", "Daga", "Lanza", "Mandoble", "Arco", "Maza",
+            "Bastón" };
+    private final String[] ARMOR_PREFIXES = { "Armadura", "Cota", "Peto", "Coraza", "Malla" };
+    private final String[] POTION_PREFIXES = { "Poción", "Elixir", "Brebaje", "Ungüento", "Tintura" };
+    private final String[] HELMET_PREFIXES = { "Yelmo", "Casco", "Celada", "Capucha", "Visera" };
+    private final String[] BOOTS_PREFIXES = { "Botas", "Grebas", "Sandalias", "Escarpines" };
 
     private final String[] NATURE_SUFIXES = {
-        "de fuego", "de hielo", "del rayo", "de la tormenta", "de la sombra", "de la luz", 
-        "de hierro", "de plata", "de obsidiana", "de acero rúnico", "de bronce antiguo",  
-        "del dragón", "del fénix", "del caos", "del vacío", "del alba", "de la luna",      
-        "del norte", "de las ruinas", "del bosque maldito", "de las profundidades", "de la montaña" 
+            "de fuego", "de hielo", "del rayo", "de la tormenta", "de la sombra", "de la luz",
+            "de hierro", "de plata", "de obsidiana", "de acero rúnico", "de bronce antiguo",
+            "del dragón", "del fénix", "del caos", "del vacío", "del alba", "de la luna",
+            "del norte", "de las ruinas", "del bosque maldito", "de las profundidades", "de la montaña"
     };
 
     private final List<String> generatedNames = new ArrayList<>();
-
 
     public Item randomItemGenerator() {
         String nameItem = "";
@@ -35,41 +35,43 @@ public class ItemGenerator {
                 case 1:
                     randomPrefixType = (int) (Math.random() * WEAPON_PREFIXES.length);
                     nameItem = WEAPON_PREFIXES[randomPrefixType];
-                    randomBasePrice = ((int) (Math.random() * 41) + 20) * 5;
-                    
+                    randomBasePrice = ((int) (Math.random() * 41) + 20) * 5; // 100 - 300
+
                     nameItem += " " + NATURE_SUFIXES[(int) (Math.random() * NATURE_SUFIXES.length)];
 
                     int randomDamage = 0;
-                    //TODO por ahora el random damage no cumple con nada establecido por los tickets de Taiga
+                    // TODO por ahora el random damage no cumple con nada establecido por los
+                    // tickets de Taiga
                     newItem = new Weapon(nameItem, randomBasePrice, randomDamage);
                     break;
 
                 case 2:
                     randomPrefixType = (int) (Math.random() * ARMOR_PREFIXES.length);
                     nameItem = ARMOR_PREFIXES[randomPrefixType];
-                    randomBasePrice = ((int) (Math.random() * 31) + 10) * 5;
-                    
+                    randomBasePrice = ((int) (Math.random() * 31) + 10) * 5; // 50 - 200
+
                     nameItem += " " + NATURE_SUFIXES[(int) (Math.random() * NATURE_SUFIXES.length)];
 
                     int randomShield = 0;
-                    //TODO por ahora el random shield no cumple con nada establecido por los tickets de Taiga
+                    // TODO por ahora el random shield no cumple con nada establecido por los
+                    // tickets de Taiga
                     newItem = new Armor(nameItem, randomBasePrice, randomShield);
                     break;
 
                 case 3:
                     randomPrefixType = (int) (Math.random() * POTION_PREFIXES.length);
                     nameItem = POTION_PREFIXES[randomPrefixType];
-                    randomBasePrice = ((int) (Math.random() * 7) + 2) * 5;
-                    
+                    randomBasePrice = ((int) (Math.random() * 7) + 2) * 5; // 10 - 40
+
                     nameItem += " " + NATURE_SUFIXES[(int) (Math.random() * NATURE_SUFIXES.length)];
-                    newItem = new Potion(nameItem, randomBasePrice); 
+                    newItem = new Potion(nameItem, randomBasePrice);
                     break;
 
                 case 4:
                     randomPrefixType = (int) (Math.random() * HELMET_PREFIXES.length);
                     nameItem = HELMET_PREFIXES[randomPrefixType];
-                    randomBasePrice = ((int) (Math.random() * 27) + 4) * 5;
-                    
+                    randomBasePrice = ((int) (Math.random() * 27) + 4) * 5; // 20 - 150
+
                     nameItem += " " + NATURE_SUFIXES[(int) (Math.random() * NATURE_SUFIXES.length)];
                     newItem = new Helmet(nameItem, randomBasePrice);
                     break;
@@ -77,8 +79,8 @@ public class ItemGenerator {
                 case 5:
                     randomPrefixType = (int) (Math.random() * BOOTS_PREFIXES.length);
                     nameItem = BOOTS_PREFIXES[randomPrefixType];
-                    randomBasePrice = ((int) (Math.random() * 17) + 4) * 5;
-                    
+                    randomBasePrice = ((int) (Math.random() * 17) + 4) * 5; // 20 - 150
+
                     nameItem += " " + NATURE_SUFIXES[(int) (Math.random() * NATURE_SUFIXES.length)];
                     newItem = new Boots(nameItem, randomBasePrice);
                     break;
@@ -91,5 +93,3 @@ public class ItemGenerator {
         return newItem;
     }
 }
-
-Contraer(93 líneas)Ite
