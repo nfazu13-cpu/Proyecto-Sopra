8738afc7d78dbfb75ed4bad3a1f97a1ede4d9187
Brian <brian.cohen@edu.gva.es>
2026-05-26 11:39:01 +0200

    ejeccio 13

---
+++ rogue-guild/src/main/java/org/sopra/rogueguild/repository/model/Other.java
@@ -0,0 +1,9 @@
+package org.sopra.rogueguild.repository.model;
+
+public class Other extends Item {
+
+    public Other(String name, int price) {
+        super(name, price, ItemCategory.OTHERS);
+    }
+
+}
