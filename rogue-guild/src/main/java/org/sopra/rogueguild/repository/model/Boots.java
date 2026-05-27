1a608a68aef55478e60a95a91050610d4dbd8242
AlejandroRueda-ctrl <alejandrorueda126@gmail.com>
2026-05-27 13:40:47 +0200

    Backup

--- rogue-guild/src/main/java/org/sopra/rogueguild/repository/model/Boots.java
+++ rogue-guild/src/main/java/org/sopra/rogueguild/repository/model/Boots.java
@@ -2,8 +2,16 @@ package org.sopra.rogueguild.repository.model;
 
 public class Boots extends Item {
     
-    public Boots(String name, int price) {
+    private int shield;
+
+    public Boots(String name, int price, int shield) {
         super(name, price, ItemCategory.BOOTS);
+        this.shield = shield;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "\nDefensa: " + this.shield;
     }
 
 }
